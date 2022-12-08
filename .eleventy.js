c7af7b4da714d475e25d44981a7e77480b856c8d
Henry Catalini Smith <henry@catalinismith.se>
2022-12-08 20:26:08 +0100

    add sass

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,16 @@
+const sass = require("sass")
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("*.webp")
+
+  eleventyConfig.addTemplateFormats("scss")
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css",
+    compile: async function(inputContent) {
+      let result = sass.compileString(inputContent)
+      return async (data) => {
+        return result.css
+      }
+    }
+  })
 }
